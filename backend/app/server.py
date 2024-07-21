a784d68ec3dc260b851e8f6c394cd3f3f9dc9d37
Gurprit Singh <Singh.Gurprit@outlook.com>
2024-07-21 00:04:35 +0000

    refactor

--- backend/app/server.py
+++ backend/app/server.py
@@ -18,7 +18,7 @@ origins = [
 # Configure CORS to allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=origins,  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
