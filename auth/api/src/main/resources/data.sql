8a6e3b6f7fd766def25f7c1ee768f933d69b4a17
Gurprit Singh <singh.gurprit@outlook.com>
2025-02-09 19:25:33 -0500

    remove auth

--- auth/api/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-delete from customer;
-insert into customer (name,email) values ('Sonali Salwan', 'ss@email.com');
-insert into customer (name,email) values ('Vaishnavi Rajendra' , 'vr@email.com');
-insert into customer (name,email) values ('Gurprit Singh'  ,'gs@email.com');
-insert into customer (name,email) values ('Gokhan Goktas' , 'jl@email.com');
-insert into customer (name,email) values ('Pablo Portillo' , 'pp@email.com');
